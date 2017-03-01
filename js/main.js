5744310d264daa4e18e3ace6a5791ad80fcaa9e3
apapa992 <andreapapageorgiou@Andreas-MacBook-Air.local>
2017-02-28 19:24:50 -0500

    jQuery Traffic Light

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+  $("#stopButton").click(illuminateRed);
+  $("#slowButton").click(illuminateYellow);
+  $("#goButton").click(illuminateGreen);
+
+  function illuminateRed() {
+    clearLights();
+    $("#stopLight").addClass("red");
+  }
+
+  function illuminateYellow() {
+    clearLights();
+    $("#slowLight").addClass("yellow");
+  }
+
+  function illuminateGreen() {
+    clearLights();
+    $("#goLight").addClass("green");
+  }
+
+  function clearLights() {
+    $(".red").removeClass("red");  
+    $(".yellow").removeClass("yellow");
+    $(".green").removeClass("green");
+  }
+
+});
