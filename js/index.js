b678375b82e6929e11329d0da3d374c4c02bb442
Miguel Hernandez <miguelhernandez@MIGUELs-MacBook-Pro.local>
2017-03-02 17:02:18 -0500

    Eureka! Commit 3
    The coding Gods are in my favor. Somewhat. HW complete.

--- js/index.js
+++ js/index.js
@@ -5,16 +5,25 @@ $(document).ready(function(){
   $('#goButton').click(switchGreen)
 
 
-  function switchRed(){
+  function switchRed() {
+    clearlights()
   $('#stopLight').css('background', 'red')
   }
 
   function switchYellow() {
+    clearlights()
   $('#slowLight').css('background', 'Yellow')
   }
 
   function switchGreen() {
+    clearlights()
   $('#goLight').css('background', 'green')
   }
-  
+
+
+  function clearlights (){
+    $('#stopLight').css('background', '#111')
+    $('#slowLight').css('background', '#111')
+    $('#goLight').css('background', '#111')
+  }
 });
