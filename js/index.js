32be194d9603c959a1df37f214ae9bc9059799e7
cliff_kuang <cliffkuang@gmail.com>
2017-03-02 08:57:35 -0500

    Created index.js

---
+++ js/index.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+  $("#stopButton").click(stopLight);
+  $("#slowButton").click(slowLight);
+  $("#goButton").click(goLight;
+
+  function stopLight() {
+    $(".bulb").css({"background-color": "red"});
+  }
+
+
+
+
+
+  });
