af154b50f4ab42147f1d3aecb8f02b4c69a658aa
cliff_kuang <cliffkuang@gmail.com>
2017-03-02 09:11:44 -0500

    Started light functionality
    began adding functions to define light actions

--- js/index.js
+++ js/index.js
@@ -2,14 +2,26 @@ $(document).ready(function(){
 
   $("#stopButton").click(stopLight);
   $("#slowButton").click(slowLight);
-  $("#goButton").click(goLight;
+  $("#goButton").click(goLight);
 
   function stopLight() {
-    $(".bulb").css({"background-color": "red"});
+    clearLights();
+    $("#stopLight").css("background-color", "red");
   }
 
+  function slowLight() {
+    clearLights ()
+    $("#slowLight").css("background-color", "yellow");
+  }
 
+  function goLight() {
+    clearLights ()
+    $("#goLight").css("background-color", "green");
+  }
 
-
+  function clearLights()
+    $("#stopLight").css("background-color", "#111");
+    $("#slowLight").css("background-color", "#111");
+    $("#goLight").css("background-color", "#111");
 
   });
