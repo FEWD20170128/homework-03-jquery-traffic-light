ed65a829ec6899c0bc8132cd303aa5e2ff87e81b
Claudia Baik <claudiabaik@gmail.com>
2017-03-03 14:32:35 -0500

    Light changes

--- js/index.js
+++ js/index.js
@@ -1,5 +1,18 @@
   function redlighton(){
-    $("#stopLight").css("background-color", "red")/*addClass("redbulb")*/;
+    $("#stopLight").css("background-color", "red");
   }
 
   $("#stopButton").click(redlighton);
+
+
+    function yellowlighton(){
+    $("#slowLight").css("background-color", "yellow");
+  }
+
+  $("#slowButton").click(yellowlighton);
+
+    function greenlighton(){
+    $("#goLight").css("background-color", "green");
+  }
+
+  $("#goButton").click(greenlighton);
